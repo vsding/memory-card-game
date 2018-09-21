2de5f5a1083ba56dd302cb2336f18d9a0ae484b3
Victoria Ding <victoriading19@gmail.com>
2018-09-21 15:27:25 -0700

    implement get_hint and variable dimension

--- memory.py
+++ memory.py
@@ -1,23 +1,28 @@
 import random, sys
 
 # Constants
-DIM = 4 # Dim must be even... eventually ask user input
-HIDDEN_CARD = 'x'
+DIM = 2# Dim must be even... eventually ask user input
+HIDDEN_CARD = '#'
 QUIT = 'q'
 HINT = 'h'
 
-# Global variables
-cards = list('1122334455667788')
-random.shuffle(cards)
-answer_board = [cards[:4], cards[4:8], cards[8:12], cards[12:]]
+card_vals = []
+for i in range(65, 65 + (DIM*DIM) / 2):
+	card_vals.append(chr(i))
+	card_vals.append(chr(i))
+
+random.shuffle(card_vals)
+answer_board = []
+for i in range(DIM):
+	answer_board.append(list(card_vals[DIM * i : DIM * (i+1)]))
 current_board = [list(HIDDEN_CARD * DIM) for i in range(DIM)]
 game_won = False
 
-def display_board(*cards):
+def display_board(*squares):
 	print('\n')
-	for row in range(len(answer_board)):
-		for col in range(len(answer_board[0])):
-			if (row, col) in cards:
+	for row in range(DIM):
+		for col in range(DIM):
+			if (row, col) in squares:
 				print(answer_board[row][col]), 
 			else:
 				print(current_board[row][col]),
@@ -30,30 +35,18 @@ def choose_card():
 	col = None
 	while row not in range(1, DIM+1) or col not in range(1, DIM+1):
 		print("Enter integers between 1 and " + str(DIM) + ":")
-		# print("[Type q to quit or h for a hint]")
+		print("(Or type q to quit or h for a hint)")
 		input_str = raw_input()
 		if input_str == QUIT:
 			quit_game()
-			return -1 # return smth different
-		# elif input_str == HINT:
-		# 	get_hint()
-		# 	return 0
-		else:
-			try:
-				row, col = map(int, input_str.split())
-			except:
-				pass
+			return -1 
+		if input_str == HINT:
+			get_hint() 
+		try:
+			row, col = map(int, input_str.split())
+		except:
+			pass
 	return (row-1, col-1)
-
-
-def quit_game():
-	print("\nThe board was:\n")
-	for row in range(len(answer_board)):
-		for col in range(len(answer_board[0])):
-			print(answer_board[row][col]), 
-		print('\n')
-	print("Good-bye!\n")
-
 
 def check_cards_match(card1, card2):
 	x1, y1 = card1
@@ -66,15 +59,37 @@ def check_cards_match(card1, card2):
 		print("Not a match - try again!\n")
 
 
+def get_hint():
+	hidden_cards = []
+	for row in range(DIM):
+		for col in range(DIM):
+			if current_board[row][col] == HIDDEN_CARD:
+				hidden_cards.append( (row, col) )
+	reveal_row, reveal_col = random.choice(hidden_cards)
+	print("\nHint: The letter " + str(answer_board[reveal_row][reveal_col]) + \
+		" appears at row " + str(reveal_row+1) + ", column " + str(reveal_col+1) + ".\n")
+
+
+def quit_game():
+	print("\nThe board was:\n")
+	for row in range(DIM):
+		for col in range(DIM):
+			print(answer_board[row][col]), 
+		print('\n')
+	print("Good-bye!\n")
+
+
 def flip_cards():	
 	card1 = choose_card()
-	if card1 == -1: # change (-1, -1) to smth else
+	if card1 == -1: # quit
 		return False
 	display_board(card1)
+
 	card2 = choose_card()
 	if card2 == -1: # quit
 		return False
 	display_board(card1, card2)
+
 	check_cards_match(card1, card2)
 
 	if any(HIDDEN_CARD in row for row in current_board): # cards remaining, keep playing
@@ -85,13 +100,15 @@ def flip_cards():
 
 
 def print_intro_rules():
-	print("\n\nWELCOME TO MEMORY\n")
-	print("Rules:")
+	print("\n\n=================")
+	print("WELCOME TO MEMORY")
+	print("=================\n")
+	print("RULES:\n")
 	print("- Whenever prompted, type the row index and column index--separated by a space--of the card you want to reveal")
 	print("- Rows and columns are 1-indexed")
 	print("  For example: 1 1 (upper leftmost card)")
 	print("               4 4 (bottom rightmost card)") 
-	print("- At any point in the game, enter q to quit")
+	print("- At any point in the game, enter q to quit or h for a hint ;)")
 
 
 def main():
@@ -103,8 +120,8 @@ def main():
 		pass
 	global game_won
 	if game_won:
-		print("Congrats!") # TODO: only print when game completed
-		print("It took you " + str(num_turns) + "turns to match all the pairs.\n")
+		print("Congrats - you've completed the game!")
+		print("It took you " + str(num_turns+1) + " turns to match all the pairs.\n")
 
 if __name__ == '__main__':
 	main()
