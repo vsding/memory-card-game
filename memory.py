3b66272d1ae6afe4682dcce95d9dc613a54f40b2
Victoria Ding <victoriading19@gmail.com>
2018-09-21 23:15:53 -0700

    prompts user to input board dimension

--- memory.py
+++ memory.py
@@ -1,17 +1,19 @@
 import random, sys
 
 # Constants
-DIM = 2# Dim must be even... eventually ask user input
 HIDDEN_CARD = '#'
 QUIT = 'q'
 HINT = 'h'
+MIN_DIMENSION = 2
+MAX_DIMENSION = 10
 
+# Global var to keep track of whether game has been won
 game_won = False
 
-def display_board(current_board, answer_board, *squares):
+def display_board(current_board, answer_board, dim, *squares):
 	print('\n')
-	for row in range(DIM):
-		for col in range(DIM):
+	for row in range(dim):
+		for col in range(dim):
 			if (row, col) in squares:
 				print(answer_board[row][col]), 
 			else:
@@ -19,24 +21,25 @@ def display_board(current_board, answer_board, *squares):
 		print('\n')
 
 
-def choose_card(current_board, answer_board):
+def choose_card(current_board, answer_board, dim):
 	print("\nType the row index and column index--separated by a space--of the card you want to flip")
 	row = None
 	col = None
-	while row not in range(1, DIM+1) or col not in range(1, DIM+1):
-		print("Enter integers between 1 and " + str(DIM) + ":")
+	while row not in range(1, dim+1) or col not in range(1, dim+1):
+		print("Enter integers between 1 and " + str(dim) + ":")
 		print("(Or type q to quit or h for a hint)")
 		input_str = raw_input()
 		if input_str == QUIT:
-			quit_game(answer_board)
-			return -1 
+			quit_game(answer_board, dim)
+			return -1 # indicates quit
 		if input_str == HINT:
-			get_hint(current_board, answer_board) 
+			get_hint(current_board, answer_board, dim) 
 		try:
 			row, col = map(int, input_str.split())
 		except:
 			pass
 	return (row-1, col-1)
+
 
 def check_cards_match(card1, card2, current_board, answer_board):
 	x1, y1 = card1
@@ -49,36 +52,36 @@ def check_cards_match(card1, card2, current_board, answer_board):
 		print("Not a match - try again!\n")
 
 
-def get_hint(current_board, answer_board):
+def get_hint(current_board, answer_board, dim):
 	hidden_cards = []
-	for row in range(DIM):
-		for col in range(DIM):
+	for row in range(dim):
+		for col in range(dim):
 			if current_board[row][col] == HIDDEN_CARD:
 				hidden_cards.append( (row, col) )
 	reveal_row, reveal_col = random.choice(hidden_cards)
-	print("\nHint: The letter " + str(answer_board[reveal_row][reveal_col]) + \
+	print("\nHint: The character " + str(answer_board[reveal_row][reveal_col]) + \
 		" appears at row " + str(reveal_row+1) + ", column " + str(reveal_col+1) + ".\n")
 
 
-def quit_game(answer_board):
+def quit_game(answer_board, dim):
 	print("\nThe board was:\n")
-	for row in range(DIM):
-		for col in range(DIM):
+	for row in range(dim):
+		for col in range(dim):
 			print(answer_board[row][col]), 
 		print('\n')
 	print("Good-bye!\n")
 
 
-def flip_cards(current_board, answer_board):	
-	card1 = choose_card(current_board, answer_board)
+def flip_cards(current_board, answer_board, dim):	
+	card1 = choose_card(current_board, answer_board, dim)
 	if card1 == -1: # quit
 		return False
-	display_board(current_board, answer_board, card1)
+	display_board(current_board, answer_board, dim, card1)
 
-	card2 = choose_card(current_board, answer_board)
+	card2 = choose_card(current_board, answer_board, dim)
 	if card2 == -1: # quit
 		return False
-	display_board(current_board, answer_board, card1, card2)
+	display_board(current_board, answer_board, dim, card1, card2)
 
 	check_cards_match(card1, card2, current_board, answer_board)
 
@@ -101,34 +104,50 @@ def print_intro_rules():
 	print("- At any point in the game, enter q to quit or h for a hint ;)")
 
 
-def init_board():
+def init_board(dim):
 	card_vals = []
-	for i in range(65, 65 + (DIM*DIM) / 2):
+	for i in range(65, 65 + (dim*dim) / 2):
 		card_vals.append(chr(i))
 		card_vals.append(chr(i))
 
 	random.shuffle(card_vals)
 	answer_board = []
-	for i in range(DIM):
-		answer_board.append(list(card_vals[DIM * i : DIM * (i+1)]))
-	current_board = [list(HIDDEN_CARD * DIM) for i in range(DIM)]
+	for i in range(dim):
+		answer_board.append(list(card_vals[dim * i : dim * (i+1)]))
+	current_board = [list(HIDDEN_CARD * dim) for i in range(dim)]
 
 	return current_board, answer_board
 
 
+def prompt_dimension():
+	print("\n\nBEFORE WE BEGIN...")
+	print("\nThe game board is an n by n square.")
+	dimension = 0
+	while dimension < MIN_DIMENSION or dimension > MAX_DIMENSION or dimension % 2 != 0:
+		print("Enter an even number n between " + str(MIN_DIMENSION) + " and " \
+			+ str(MAX_DIMENSION) + ", inclusive:")
+		dimension = raw_input()
+		try:
+			dimension = int(dimension)
+		except:
+			pass
+	return dimension
+
 def main():
-	current_board, answer_board = init_board()
+	print_intro_rules()
+	dimension = prompt_dimension()
+	current_board, answer_board = init_board(dimension)
 	game_won = False
 	num_turns = 0
-	print_intro_rules()
-	display_board(current_board, answer_board)
-	while flip_cards(current_board, answer_board):
+	display_board(current_board, answer_board, dimension)
+	while flip_cards(current_board, answer_board, dimension):
 		num_turns += 1
 		pass
 	global game_won
 	if game_won:
 		print("Congrats - you've completed the game!")
 		print("It took you " + str(num_turns+1) + " turns to match all the pairs.\n")
+
 
 if __name__ == '__main__':
 	main()
